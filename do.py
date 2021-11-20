3bcd0e747f712df5ef92454b783913f743ffce31
retnrernet <49436790+retnrernet@users.noreply.github.com>
2021-11-20 00:05:12 +0000

    Update do.py

--- do.py
+++ do.py
@@ -7,13 +7,7 @@ def set_hook():
     from aiogram import Bot
     bot = Bot(token=BOT_TOKEN)
 
-    async def hook_set():
-        if not HEROKU_APP_NAME:
-            print('You have forgot to set HEROKU_APP_NAME')
-            quit()
-        await bot.set_webhook(WEBHOOK_URL)
-        print(await bot.get_webhook_info())
-    
+
 
     asyncio.run(hook_set())
     bot.close()
